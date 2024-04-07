c097419a2eec1ff6eb0dec9b06cb014199d3b6e9
jantiojo <jonelantiojo.work@gmail.com>
2024-04-07 15:16:49 +0800

    #added Expanded Car Item Composable

--- app/src/main/java/com/jantiojo/guidomia/ui/car/CarItem.kt
+++ app/src/main/java/com/jantiojo/guidomia/ui/car/CarItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Divider
@@ -19,14 +18,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jantiojo.guidomia.R
 import com.jantiojo.guidomia.ui.model.CarItemUiModel
 import com.jantiojo.guidomia.ui.theme.Black45
@@ -35,10 +41,11 @@ import com.jantiojo.guidomia.ui.theme.GuidomiaTheme
 import com.jantiojo.guidomia.ui.theme.Orange
 import com.jantiojo.guidomia.ui.theme.Sizes
 
+private const val bullet = "\u2022"
+
 @Composable
 fun CarItem(
-    carItemUiModel: CarItemUiModel,
-    modifier: Modifier = Modifier
+    carItemUiModel: CarItemUiModel, modifier: Modifier = Modifier
 ) {
 
     Column(modifier = modifier.fillMaxWidth()) {
@@ -61,8 +68,7 @@ fun CarItem(
 
                 Column(modifier = Modifier.fillMaxWidth()) {
                     Text(
-                        text = carItemUiModel.name,
-                        style = MaterialTheme.typography.bodyLarge.copy(
+                        text = carItemUiModel.name, style = MaterialTheme.typography.bodyLarge.copy(
                             color = Black45,
                             fontWeight = FontWeight.Bold,
                             platformStyle = PlatformTextStyle(
@@ -72,10 +78,8 @@ fun CarItem(
                     )
                     Text(
                         text = stringResource(
-                            id = R.string.price_label,
-                            carItemUiModel.priceDisplay
-                        ),
-                        style = MaterialTheme.typography.labelSmall.copy(
+                            id = R.string.price_label, carItemUiModel.priceDisplay
+                        ), style = MaterialTheme.typography.labelSmall.copy(
                             color = Black45,
                             fontWeight = FontWeight.Bold,
                             platformStyle = PlatformTextStyle(
@@ -100,6 +104,12 @@ fun CarItem(
                     }
                 }
             }
+
+            CarExpandedUI(
+                pros = carItemUiModel.pros,
+                cons = carItemUiModel.cons,
+                modifier = Modifier.padding(Sizes.Medium)
+            )
         }
 
         Divider(
@@ -113,6 +123,70 @@ fun CarItem(
     }
 }
 
+
+@Composable
+private fun CarExpandedUI(
+    pros: List<String>, cons: List<String>, modifier: Modifier = Modifier
+) {
+    val paragraphStyle = ParagraphStyle(textIndent = TextIndent(restLine = 12.sp))
+
+    Column(
+        modifier = modifier, verticalArrangement = Arrangement.spacedBy(Sizes.Small)
+    ) {
+        Text(buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Black45, fontWeight = FontWeight.Bold)) {
+                append(stringResource(id = R.string.pros_label))
+            }
+
+            pros.forEach {
+                withStyle(style = paragraphStyle) {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Orange, fontWeight = FontWeight.Bold, fontSize = 18.sp
+                        )
+                    ) {
+                        append("\t\t" + bullet)
+                    }
+                    append("\t\t")
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black, fontWeight = FontWeight.Bold, fontSize = 14.sp
+                        )
+                    ) {
+                        append(it)
+                    }
+                }
+            }
+
+        })
+
+        Text(text = buildAnnotatedString {
+
+            withStyle(style = SpanStyle(color = Black45, fontWeight = FontWeight.Bold)) {
+                append(stringResource(id = R.string.cons_label))
+            }
+            cons.forEach {
+                withStyle(style = paragraphStyle) {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Orange, fontWeight = FontWeight.Bold, fontSize = 18.sp
+                        )
+                    ) {
+                        append("\t\t" + bullet)
+                    }
+                    append("\t\t")
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black, fontWeight = FontWeight.Bold, fontSize = 14.sp
+                        )
+                    ) {
+                        append(it)
+                    }
+                }
+            }
+        })
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
