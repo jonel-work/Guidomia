67443ccbca988d4da4a8409cd8235a07878432d7
jantiojo <jonelantiojo.work@gmail.com>
2024-04-07 11:11:37 +0800

    #added color and size requirements

--- app/src/main/java/com/jantiojo/guidomia/ui/theme/Color.kt
+++ app/src/main/java/com/jantiojo/guidomia/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFFC6016)
+val DarkGray = Color(0xFF858585)
+val LightGray = Color(0xFFD5D5D5)
+val Black45 = Color(0x73000000)
+val Black = Color.Black
---
+++ app/src/main/java/com/jantiojo/guidomia/ui/theme/Sizes.kt
@@ -0,0 +1,57 @@
+package com.jantiojo.guidomia.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object Sizes {
+
+    /**
+     * <dimen name="size_none">0dp</dimen>
+     */
+    val None = 0.dp
+
+    /**
+     * <dimen name="size_xxs">2dp</dimen>
+     */
+    val ExtraExtraSmall = 2.dp
+
+    /**
+     * <dimen name="size_xs">4dp</dimen>
+     */
+    val ExtraSmall = 4.dp
+
+    /**
+     * <dimen name="size-s">8dp</dimen>
+     */
+    val Small = 8.dp
+
+    /**
+     * <dimen name="size-m">12dp</dimen>
+     */
+    val Medium = 12.dp
+
+    /**
+     * <dimen name="size-l">16dp</dimen>
+     */
+    val Large = 16.dp
+
+    /**
+     * <dimen name="size-xl">24dp</dimen>
+     */
+    val ExtraLarge = 24.dp
+
+    /**
+     * <dimen name="size-xxl">32dp</dimen>
+     */
+    val ExtraExtraLarge = 32.dp
+
+    /**
+     * <dimen name="size-xxl">64dp</dimen>
+     */
+    val ExtraExtraExtraLarge = 64.dp
+
+    /**
+     * default size based on the requirements
+     */
+    val LineSize: Dp = 4.dp
+}
